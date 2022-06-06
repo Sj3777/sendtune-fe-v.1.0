83635401785de46059a7e193b84f278a4aacf998
Sj3777 <sudhakarjha819@gmail.com>
2022-06-06 22:56:50 +0530

    color-update

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -41,7 +41,7 @@ function Login() {
           <div className="flex justify-center items-center">
             <button
               type="submit"
-              className="w-40 py-3 px-4 mt-12 border border-transparent text-sm font-medium rounded-md text-white bg-red-500 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              className="w-40 py-3 px-4 mt-12 border border-transparent text-sm font-medium rounded-md text-white bg-red-500 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
               <span className="absolute left-0 inset-y-0 flex items-center pl-3">
                 {/* <LockClosedIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" /> */}
